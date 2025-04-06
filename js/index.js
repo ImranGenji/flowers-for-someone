43d954eecdf5460934cd8f55adb01b66b779b394
imran <imrangenji05@gmail.com>
2025-04-06 20:08:13 +0800

    change for audrey

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Hey My Sweet Bbg, I Miss You'.split('')
+const text = 'AUDREY MAGANDA BABY'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('LOVE U ANGEL Bbg').split('')
+    const titles = ('love u baddie').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
